0163571cdbf3de0b27a5b5363db68ca99d82ff79
Mao Chuan Li <mao.chuan.li@gmail.com>
2015-10-19 20:18:37 +1300

    add the build script in main project
    Body
    
    Signed-off-by: Mao Chuan Li <mao.chuan.li@gmail.com>

---
+++ saas-on-osgi-build.sh
@@ -0,0 +1,25 @@
+echo "Build the main system..."
+cd saas-on-osgi-main/
+mvn clean install
+
+echo
+echo "Build the depenancy services..."
+echo
+cd ../saas-on-osgi-services/
+cd embedded-jetty/
+mvn clean install
+
+cd ../java.heroku.proxy.bundle/
+mvn clean install
+
+cd ../com.sa.osgi.ui/
+mvn clean install
+
+cd ../T1.com.sa.osgi.ui/
+mvn clean install
+
+cd ../T3.com.sa.osgi.ui/
+mvn clean install
+echo 
+echo "DONE building all service bundles..."
+echo
